3e178fe093f9e4b7e6b637befbed3534ff5820b6
Jashem <jashemaits21@gmail.com>
2022-04-19 15:05:28 +0600

    lecture 1

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Mohammad Jashem on 4/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+                
+                
+            Text("Hello, world!")
+                .foregroundColor(.orange)
+                .padding()
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+            
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mohammad Jashem on 4/19/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
